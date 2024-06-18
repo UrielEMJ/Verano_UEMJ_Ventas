633c2e073d8f01efe4c3fba45d23a67116f7da28
UrielMarquez01 <20030636@itesa.edu.mx>
2024-06-18 11:51:35 -0600

    Veersion07. CRUD Paises completo.

--- Sales.WEB/Repositories/IRepository.cs
+++ Sales.WEB/Repositories/IRepository.cs
@@ -2,8 +2,23 @@
 {
     public interface IRepository
     {
+        //Get
         Task<HttpResponseWrapper<T>> Get<T>(string url);
+        //Post
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+        //Post que devuelve respuesta
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+        //Delete
+        Task<HttpResponseWrapper<object>> Delete(string url);
+
+        //Put
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        //Put que devuelve una respuesta
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
+
+
+
     }
 }
--- Sales.WEB/Repositories/Repository.cs
+++ Sales.WEB/Repositories/Repository.cs
@@ -62,5 +62,33 @@ namespace Sales.WEB.Repositories
             return JsonSerializer.Deserialize<T>(respuestaString,
             jsonSerializerOptions)!;
         }
+        //Delete
+        public async Task<HttpResponseWrapper<object>> Delete(string url)
+        {
+            var responseHTTP = await _httpClient.DeleteAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
+        }
+        //Metodo Put que no devuelve una respuesta
+        public async Task<HttpResponseWrapper<object>> Put<T>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContent);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+        //Metodo Put que si devuelve una respuesta
+        public async Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContent);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswer<TResponse>(responseHttp, _jsonDefaultOptions);
+                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<TResponse>(default, !responseHttp.IsSuccessStatusCode,
+           responseHttp);
+        }
     }
 }
